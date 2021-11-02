aa0ff063d020514d1514aeffb6624f09b72596ae
Jules <jules.houben@live.nl>
2021-11-02 10:34:56 +0100

    Added CRUD

--- app/routes/floor.routes.js
+++ app/routes/floor.routes.js
@@ -2,7 +2,24 @@ module.exports = app => {
     const floors = require('../controllers/floor.controller.js');
 
     var router = require("express").Router();
+    
+    // Create a new floor
     router.post("/", floors.create);
+
+    // Retrieve all floors
+    router.get("/", floors.findAll);
+
+    // Retrieve a single floor by id
+    router.get("/:id", floors.findOne);
+
+    // Update a floor by id
+    router.put("/:id", floors.update);
+
+    // Delete a floor by id
+    router.delete("/:id", floors.delete);
+
+    // Delete all floors
+    router.delete("/", floors.deleteAll);
 
     app.use('/api/floors', router);
 };
