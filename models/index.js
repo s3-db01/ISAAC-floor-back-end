21e54f040576b66e1181a4a34400d3eed93b1437
Jules <jules.houben@live.nl>
2021-11-08 10:45:51 +0100

    Updated index.js

--- models/index.js
+++ models/index.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-let env = process.env.NODE_ENV || 'development';
-process.env.NODE_ENV = 'test';
-env = process.env.NODE_ENV;
-const config = require(__dirname + './../config/config.js')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '../config/config.js')[env];
 const db = {};
 
 let sequelize;
