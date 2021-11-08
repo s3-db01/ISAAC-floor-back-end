2434e5fe66be205d618d4cdb4c2e89d873a190be
Jules <jules.houben@live.nl>
2021-11-08 10:19:30 +0100

    Updated index.js

--- models/index.js
+++ models/index.js
@@ -10,6 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log('Using environment variable');
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   console.log('im triggered');
